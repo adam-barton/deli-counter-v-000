c1ec1c9a9c6ca1d04263830eae8117c6a5f48624
Adam Barton <adam.barton333@gmail.com>
2018-05-16 02:38:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
- if katz_deli.empty?
+ if Array.empty?
    puts "There is nobody waiting to be served!"
  else
   puts "Now serving "
